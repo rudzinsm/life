71e910bc19f56f1ce8256011c3d295088fc27ebe
Maciej Rudziński <rudzinski.maciek@gmail.com>
2020-01-20 21:42:00 +0100

    Board first implementation

--- src/main/java/com/maciej/life/Board.java
+++ src/main/java/com/maciej/life/Board.java
@@ -1,4 +1,45 @@
 package com.maciej.life;
 
 public class Board {
+    private final int width;
+    private final int height;
+    private boolean[][] values;
+
+    public Board(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public boolean getField(int row, int column) {
+        return values[row][column];
+    }
+
+    public void setField(int row, int column, boolean value) {
+        this.values = values;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(int r = 0 ; r < height; r++) {
+            sb.append('\n');
+            for(int c = 0 ; c < width; c++) {
+                sb.append(values[r][c] ?"#":".");
+            }
+
+        }
+
+        return sb.toString();
+    }
+
+
+
 }
